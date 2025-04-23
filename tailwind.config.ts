cc0cafa769e9fd4a0e1a2311a266bcbc1289479f
Felix Cruz <fcruzm95@gmail.com>
2025-04-23 15:02:16 +0100

    fixed lint errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import { type Config } from "tailwindcss";
-import fontFamily from "tailwindcss/defaultTheme";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+const { fontFamily } = defaultTheme;
 
 export default {
 	darkMode: "class",
